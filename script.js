0d2170d9ce49182bfc01f1518aaa3020f720d2a1
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-02-24 19:19:11 +0100

    if

--- script.js
+++ script.js
@@ -8,4 +8,8 @@ for(i =1; i<= 100; i++){
     let newDiv = document.createElement('div');
     newDiv.innerHTML = i;
     daje.append(newDiv);
+
+    if( i % 3 == 0 ){
+        newDiv.innerHTML = Fizz;
+    }
 }
