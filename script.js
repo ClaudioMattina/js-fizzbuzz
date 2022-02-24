5a9d6d45fcab002c91124e9ef99be77ff6954f69
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-02-24 19:24:31 +0100

    ci siamo quasi

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ for(i =1; i<= 100; i++){
     daje.append(newDiv);
 
     if( i % 3 == 0 ){
-        newDiv.innerHTML = Fizz;
+        newDiv.innerHTML = "Fizz";
+    }
+    else if( i % 5 == 0){
+        newDiv.innerHTML = "Buzz";
+    }
+    else if( i % 3 == 0 & i % 5 == 0) { 
+        newDiv.innerHTML = "FizzBuzz";
     }
 }
