afe9e2d3bd3105e4ca33a21c9353a4c9e0ea58b8
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-02-25 16:25:21 +0100

    piccola correzione

--- script.js
+++ script.js
@@ -3,20 +3,26 @@
 const daje = document.getElementById("ciao");
 
 
-for(i =1; i<= 100; i++){
+for(i = 1; i <= 100; i++){
 
     let newDiv = document.createElement('div');
     newDiv.innerHTML = i;
     newDiv.classList.add("box")
-    daje.append(newDiv);
+    
 
-    if( i % 3 == 0 ){
-        newDiv.innerHTML = "Fizz";
-    }
-    else if( i % 5 == 0){
-        newDiv.innerHTML = "Buzz";
-    }
-    else if( i % 3 == 0 && i % 5 == 0) { 
+    if( i % 15 == 0 ){
         newDiv.innerHTML = "FizzBuzz";
     }
-}
+    else if( i % 3 == 0){
+        newDiv.innerHTML = "Fizz";
+    }
+    else if( i % 5 == 0) { 
+        newDiv.innerHTML = "Buzz";
+    
+    }
+    else {
+        newDiv.innerHTML = i;
+    }
+
+    daje.append(newDiv);
+};
